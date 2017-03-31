ec56ea425ba03f4e84332dd02918daf4b53bdef5
nick <crismas90@gmail.com>
2017-03-31 21:15:35 +0400

    min,max -work sum ????

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-﻿
+﻿function min(array) {
+  if (typeof array !== 'undefined') {
+  if(!array.length){
+  return undefined;
+  }
+  var numFilter = array.filter(function(i){
+  if(typeof i ==='number' && !isNaN(i)){
+  return true;
+  }
+  else {
+  return false;
+  }
+  });
+ return Math.min.apply(null, numFilter);
+ }
+}
+function max(array) {
+  if (typeof array !== 'undefined') {
+  if(!array.length){
+  return undefined;
+  }
+  var numFilter = array.filter(function(i){
+  if(typeof i ==='number' && !isNaN(i)){
+  return true;
+  }
+  else {
+  return false;
+  }
+  });
+ return Math.max.apply(null, numFilter);
+ }
+}
+
+function sum() {
+  var result = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    result += arguments[i];
+  }
+
+  return result;
+}
