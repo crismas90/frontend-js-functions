7abf080e3c2bb9ed44ead9e8c7431b71a8aa41cc
nick <crismas90@gmail.com>
2017-04-01 00:19:58 +0400

    sum typeof

--- src/index.js
+++ src/index.js
@@ -32,10 +32,11 @@ function max(array) {
 }
 
 function sum() {
-  var result = 0;
+  var sumNum = 0;
   for (var i = 0; i < arguments.length; i++) {
-    result += arguments[i];
-  }
-
-  return result;
+    if (typeof arguments[i] === "number" && isNaN(arguments[i]) !== true){
+    sumNum += arguments[i];
+    }
+  };
+  return sumNum;
 }
