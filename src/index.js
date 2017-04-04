87678951a8a5330b01f65fee0769a998a9bbf71a
nick <crismas90@gmail.com>
2017-04-04 20:45:23 +0300

    Indents in the code

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 ﻿function min(array) {
   if (typeof array !== 'undefined') {
-  if(!array.length){
-  return undefined;
+    if (!array.length) {
+      return undefined;
+    }
+    var numFilter = array.filter(function (i) {
+      if (typeof i === 'number' && !isNaN(i)) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    });
+    return Math.min.apply(null, numFilter);
   }
-  var numFilter = array.filter(function(i){
-  if(typeof i ==='number' && !isNaN(i)){
-  return true;
-  }
-  else {
-  return false;
-  }
-  });
- return Math.min.apply(null, numFilter);
- }
 }
 function max(array) {
   if (typeof array !== 'undefined') {
-  if(!array.length){
-  return undefined;
+    if (!array.length) {
+      return undefined;
+    }
+    var numFilter = array.filter(function (i) {
+      if (typeof i === 'number' && !isNaN(i)) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    });
+    return Math.max.apply(null, numFilter);
   }
-  var numFilter = array.filter(function(i){
-  if(typeof i ==='number' && !isNaN(i)){
-  return true;
-  }
-  else {
-  return false;
-  }
-  });
- return Math.max.apply(null, numFilter);
- }
 }
 
 function sum() {
   var sumNum = 0;
   for (var i = 0; i < arguments.length; i++) {
-    if (typeof arguments[i] === "number" && isNaN(arguments[i]) !== true){
-    sumNum += arguments[i];
+    if (typeof arguments[i] === "number" && isNaN(arguments[i]) !== true) {
+      sumNum += arguments[i];
     }
   };
   return sumNum;
